c84add2a7e885615d38ded450fe125e6fce2e1fc
Phong Chu <chucamphong1999@gmail.com>
2026-03-01 13:23:47 +0700

    chore: add Prettier configuration file and update smoke test URL

--- tests/apps/desk/smoke.spec.ts
+++ tests/apps/desk/smoke.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('We can access Desk application', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveURL(/^https?:\/\/desk\./);
+  await expect(page).toHaveURL(/^https?:\/\/workastra.com/);
 });
